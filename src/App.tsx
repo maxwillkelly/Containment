da4ffc92e27939aeb3c3d80ae837207b0b2de2e0
Max Kelly <maxwill.kellk@gmail.com>
2021-02-17 01:30:58 +0000

    Started start menu

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Start from './pages/Start';
 import './App.global.css';
+import Game from './pages/Game';
+import Settings from './pages/Settings';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" component={Start} />
+        <Route path="/game" component={Game} />
+        <Route path="/settings" component={Settings} />
       </Switch>
     </Router>
   );
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+interface MenuItemProps {
+  name: string;
+  path: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = ({ name, path }) => {
+  return (
+    <div className="w-full p-5 text-4xl text-center">
+      <Link to={path}>{name}</Link>
+    </div>
+  );
+};
 
 const Start: React.FC = () => {
   return (
-    <div>
-      <h1>Start</h1>
-      <div className="bg-gray-500 p-5 text-center">Tailwind</div>
+    <div className="h-screen w-screen px-32 py-36">
+      <div className="bg-gray-500 h-full w-full">
+        <h1 className="py-12 text-5xl text-center">Containment</h1>
+        <MenuItem name="New Game" path="/game" />
+        <MenuItem name="Load Game" path="/" />
+        <MenuItem name="Settings" path="/settings" />
+      </div>
     </div>
   );
 };
