d870a877acd2d910815e01b463eb5c9f8b99ad78
Max Kelly <maxwill.kellk@gmail.com>
2021-02-21 23:15:08 +0000

    Working routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Start from './pages/Start';
-import './App.global.css';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import './App.global.css';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Start} />
+        <Route exact path="/" component={Start} />
         <Route path="/game" component={Game} />
         <Route path="/settings" component={Settings} />
       </Switch>
--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Settings: React.FC = () => {
   return (
     <div>
       <h1>Settings</h1>
+      <Link to="/">Back to Main Menu</Link>
     </div>
   );
 };
