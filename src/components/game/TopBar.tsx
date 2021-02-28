ceb33a4b9e0f92e9a1f887425cd45fc33104732f
Max Kelly <maxwill.kelly@gmail.com>
2021-02-28 23:02:07 +0000

    Adds empty top bar

--- src/components/game/TopBar.tsx
+++ src/components/game/TopBar.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const TopBar: React.FC = () => {
-  return null;
+  return (
+    <nav className="mx-auto p-4 dark:bg-gray-700 dark:text-gray-200">
+      <div className="flex flex-row justify-between">
+        <h1>X</h1>
+        <h1>Y</h1>
+      </div>
+    </nav>
+  );
 };
 
 export default TopBar;
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import TopBar from '../components/game/TopBar';
 
 const Game: React.FC = () => {
   return (
     <div>
-      <h1>Game</h1>
-      <Link to="/">Back to Main Menu</Link>
+      <TopBar />
+      <Link className="m-5" to="/">
+        Back to Main Menu
+      </Link>
     </div>
   );
 };
