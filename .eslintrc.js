49255b9247057579e7dc0c2c5a3fd95b34b12bcb
Max Kelly <maxwill.kellk@gmail.com>
2021-02-28 01:12:20 +0000

    Revert "Disables flow errors"
    This reverts commit 462f1f420a871d3f339b9217f7083c4093975a43.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
     'react/prop-types': 0,
-    'flowtype-errors/show-errors': 0,
   },
   parserOptions: {
     ecmaVersion: 2020,
