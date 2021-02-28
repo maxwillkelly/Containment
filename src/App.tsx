5b0d8428208675cdd46070bef6b121bcc76b607f
Max Kelly <maxwill.kelly@gmail.com>
2021-02-28 20:17:47 +0000

    Tried to put postcss into sass

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Start from './pages/Start';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+
 import './App.global.css';
+import './styles/background.scss';
 
 const App: React.FC = () => {
   return (
