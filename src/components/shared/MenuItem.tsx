4a4f8523bef5f635549f7d6455344d3cc6f78c47
Max Kelly <maxwill.kelly@gmail.com>
2021-03-03 17:41:09 +0000

    Refactoring in Menu Item

--- src/components/shared/MenuItem.tsx
+++ src/components/shared/MenuItem.tsx
@@ -8,21 +8,18 @@ interface Props {
 }
 
 const MenuItem: React.FC<Props> = ({ name, path, handleClick }) => {
-  if (path) {
+  const styles =
+    'w-full p-5 text-2xl shadow-2xl dark:bg-gray-700 dark:text-gray-200 mb-4 text-center flex-shrink';
+
+  if (path)
     return (
       <Link to={path}>
-        <div className="p-5 text-2xl shadow-2xl dark:bg-gray-700 dark:text-gray-200 mb-4 text-center flex-shrink">
-          {name}
-        </div>
+        <div className={styles}>{name}</div>
       </Link>
     );
-  }
+
   return (
-    <button
-      className="w-full p-5 text-2xl shadow-2xl dark:bg-gray-700 dark:text-gray-200 mb-4 text-center flex-shrink"
-      type="button"
-      onClick={handleClick}
-    >
+    <button className={styles} type="button" onClick={handleClick}>
       {name}
     </button>
   );
