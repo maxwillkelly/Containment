5ad969dedd058b520d5b3522be1c70f1fdcd66cb
Max Kelly <maxwill.kelly@gmail.com>
2021-03-27 19:08:35 +0000

    Removes unnecessary map styling

--- src/components/game/Map.tsx
+++ src/components/game/Map.tsx
@@ -19,18 +19,6 @@ const Map: React.FC = () => {
   // Initialize map
   useEffect(() => {
     const styles: Record<string, Style> = {
-      LineString: new Style({
-        stroke: new Stroke({
-          color: 'green',
-          width: 1,
-        }),
-      }),
-      MultiLineString: new Style({
-        stroke: new Stroke({
-          color: 'green',
-          width: 1,
-        }),
-      }),
       MultiPolygon: new Style({
         stroke: new Stroke({
           color: 'yellow',
@@ -38,34 +26,6 @@ const Map: React.FC = () => {
         }),
         fill: new Fill({
           color: 'rgba(255, 255, 0, 0.1)',
-        }),
-      }),
-      Polygon: new Style({
-        stroke: new Stroke({
-          color: 'blue',
-          lineDash: [4],
-          width: 3,
-        }),
-        fill: new Fill({
-          color: 'rgba(0, 0, 255, 0.1)',
-        }),
-      }),
-      GeometryCollection: new Style({
-        stroke: new Stroke({
-          color: 'magenta',
-          width: 2,
-        }),
-        fill: new Fill({
-          color: 'magenta',
-        }),
-      }),
-      Circle: new Style({
-        stroke: new Stroke({
-          color: 'red',
-          width: 2,
-        }),
-        fill: new Fill({
-          color: 'rgba(255,0,0,0.2)',
         }),
       }),
     };
