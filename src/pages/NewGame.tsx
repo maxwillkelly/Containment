b5e343ed9f491ce77ceecaced3850b553d9e7350
Max Kelly <maxwill.kelly@gmail.com>
2021-04-04 16:49:14 +0100

    Fixes paused on new game bug

--- src/pages/NewGame.tsx
+++ src/pages/NewGame.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
 import GameWindow from '../components/shared/GameWindow';
+import WindowButton from '../components/shared/WindowButton';
 import WindowLink from '../components/shared/WindowLink';
+import useGameStore from '../stores/GameStore';
 
-const Footer: React.FC = () => (
-  <>
-    <WindowLink title="Start" path="/game" />
-    <WindowLink title="Cancel" path="/" />
-  </>
-);
+const Footer: React.FC = () => {
+  const history = useHistory();
+  const togglePause = useGameStore((state) => state.togglePause);
 
-const NewGame: React.FC = () => (
-  <GameWindow title="New game" footer={<Footer />} />
-);
+  const handleStart = () => {
+    togglePause();
+    history.push('/game');
+  };
+
+  return (
+    <>
+      <WindowButton title="Start" handleClick={handleStart} />
+      <WindowLink title="Cancel" path="/" />
+    </>
+  );
+};
+
+const NewGame: React.FC = () => {
+  return <GameWindow title="New game" footer={<Footer />} />;
+};
 
 export default NewGame;
