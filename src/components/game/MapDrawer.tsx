f137461e506b6b8397062e902b56e125beae55e1
Max Kelly <maxwill.kellk@gmail.com>
2021-04-10 21:51:54 +0100

    Sets up how viral data will be displayed

--- src/components/game/MapDrawer.tsx
+++ src/components/game/MapDrawer.tsx
@@ -2,6 +2,42 @@ import React from 'react';
 import useMapStore from '../../stores/MapStore';
 import useViralStore from '../../stores/ViralStore';
 
+const DashedHeading: React.FC = ({ children }) => (
+  <h2 className="text-sm dashed-heading text-center text-gray-300">
+    {children}
+  </h2>
+);
+
+const InformationList: React.FC = ({ children }) => (
+  <div className="grid grid-cols-2 grid-flow-row gap-y-3 px-4 py-5 text-sm">
+    {children}
+  </div>
+);
+
+const NationalDetails: React.FC = () => {
+  return (
+    <>
+      <DashedHeading>National Details</DashedHeading>
+      <InformationList>
+        <h4 className="text-left">Population</h4>
+        <h4 className="text-right">923 million</h4>
+        <h4 className="text-left">Capital</h4>
+        <h4 className="text-right">Denver</h4>
+        <h4 className="text-left">Largest city</h4>
+        <h4 className="text-right">Denver</h4>
+        <h4 className="text-left">Largest state</h4>
+        <h4 className="text-right">Los Almos</h4>
+        <h4 className="text-left">National language</h4>
+        <h4 className="text-right">Planetarian</h4>
+        <h4 className="text-left">Currency</h4>
+        <h4 className="text-right">Planetarian Pound</h4>
+        <h4 className="text-left">Driving side</h4>
+        <h4 className="text-right">Left</h4>
+      </InformationList>
+    </>
+  );
+};
+
 const StateDetails: React.FC = () => {
   const selectedState = useMapStore((state) => state.selectedState);
 
@@ -9,15 +45,44 @@ const StateDetails: React.FC = () => {
 
   return (
     <>
-      <h2 className="text-sm dashed-heading text-center text-gray-300">
-        State Details
-      </h2>
-      <div className="grid grid-cols-2 grid-flow-row px-4 py-5 text-sm">
+      <DashedHeading>State Details</DashedHeading>
+      <InformationList>
         <h4 className="text-left">Population</h4>
         <h4 className="text-right">
           {`${selectedState.population / 1000000} million`}
         </h4>
-      </div>
+      </InformationList>
+    </>
+  );
+};
+
+const ViralDetails: React.FC = () => {
+  // const selectedState = useMapStore((state) => state.selectedState);
+
+  return (
+    <>
+      <DashedHeading>Weekly Data</DashedHeading>
+      <InformationList>
+        <h4 className="text-left text-base text-yellow-300">Cases</h4>
+        <h4 className="text-right">0</h4>
+        <h4 className="text-left text-base text-red-500">Deaths</h4>
+        <h4 className="text-right">0</h4>
+        <h4 className="text-left text-base text-green-500">Recovered</h4>
+        <h4 className="text-right">0</h4>
+        <h4 className="text-left text-base text-blue-500">Innoculated</h4>
+        <h4 className="text-right">0</h4>
+      </InformationList>
+      <DashedHeading>Cumalitive Data</DashedHeading>
+      <InformationList>
+        <h4 className="text-left text-base text-yellow-300">Cases</h4>
+        <h4 className="text-right">0</h4>
+        <h4 className="text-left text-base text-red-500">Deaths</h4>
+        <h4 className="text-right">0</h4>
+        <h4 className="text-left text-base text-green-500">Recovered</h4>
+        <h4 className="text-right">0</h4>
+        <h4 className="text-left text-base text-blue-500">Innoculated</h4>
+        <h4 className="text-right">0</h4>
+      </InformationList>
     </>
   );
 };
@@ -32,23 +97,21 @@ const DebugDetails: React.FC = () => {
 
   return (
     <>
-      <h2 className="text-sm dashed-heading text-center text-gray-300">
-        Debug
-      </h2>
-      <div className="grid grid-cols-2 grid-flow-row px-4 py-5 text-sm">
+      <DashedHeading>Debug</DashedHeading>
+      <InformationList>
         <h4 className="text-left">Person Machines</h4>
         <h4 className="text-right">{getMachinesTotal(selectedState.name)}</h4>
         <pre>
           {JSON.stringify(getMachinesStates(selectedState.name), null, 2)}
         </pre>
-      </div>
-      <div className="grid grid-cols-2 grid-flow-row px-4 py-5 text-sm">
+      </InformationList>
+      <InformationList>
         <h4 className="text-left">Viral Details</h4>
         <h4 className="text-right">{}</h4>
         <pre>
           {JSON.stringify(getViralDetails(selectedState.name), null, 2)}
         </pre>
-      </div>
+      </InformationList>
     </>
   );
 };
@@ -61,14 +124,15 @@ const MapDrawer: React.FC = () => {
 
   return (
     <aside
-      className={`transform right-0 w-64 dark:bg-gray-700 dark:text-gray-200 border-l border-t border-gray-900 shadow fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${asideClass}`}
+      className={`transform right-0 w-72 dark:bg-gray-700 dark:text-gray-200 border-l border-t border-gray-900 shadow fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${asideClass}`}
     >
       <div className="flex flex-col justify-center">
         <h3 className="text-lg px-4 py-5">
-          {selectedState ? selectedState.name : ''}
+          {selectedState ? selectedState.name : 'The United Planet'}
         </h3>
       </div>
-      <StateDetails />
+      <ViralDetails />
+      {selectedState ? <StateDetails /> : <NationalDetails />}
       <DebugDetails />
     </aside>
   );
