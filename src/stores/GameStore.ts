4d4a1ff181b257e3877a9d0ff38098b0c3f29fae
Max Kelly <maxwill.kellk@gmail.com>
2021-04-10 17:21:14 +0100

    Splits Map Store from Game Store

--- src/stores/GameStore.ts
+++ src/stores/GameStore.ts
@@ -1,26 +1,13 @@
 import create from 'zustand';
 
-type ResidentStateDetails = {
-  fid: number;
-  abbreviation: number;
-  name: string;
-  population: number;
-};
-
 type State = {
   turn: number;
   loading: boolean;
-  isActionDrawerOpen: boolean;
-  isMapDrawerOpen: boolean;
-  selectedState: ResidentStateDetails | undefined;
   isPaused: boolean;
   activeApplet: string;
 
   advanceTurn: () => void;
   setLoading: (value: boolean) => void;
-  toggleActionDrawer: () => void;
-  toggleMapDrawer: (open?: boolean) => void;
-  selectState: (selectedState?: ResidentStateDetails) => void;
   togglePause: () => void;
   toggleActiveApplet: (applet: string) => void;
   reset: () => void;
@@ -30,24 +17,12 @@ const useGameStore = create<State>((set) => ({
   turn: 0,
   loading: true,
   isPaused: true,
-  isActionDrawerOpen: false,
-  isMapDrawerOpen: false,
-  selectedState: undefined,
   activeApplet: '',
 
   advanceTurn: () => set((state) => ({ turn: state.turn + 1 })),
 
   setLoading: (value) => set(() => ({ loading: value })),
 
-  toggleActionDrawer: () =>
-    set((state) => ({ isActionDrawerOpen: !state.isActionDrawerOpen })),
-
-  toggleMapDrawer: (open?: boolean) =>
-    set((state) => ({
-      isMapDrawerOpen: open === undefined ? !state.isMapDrawerOpen : open,
-    })),
-
-  selectState: (selectedState) => set(() => ({ selectedState })),
   togglePause: () => set((state) => ({ isPaused: !state.isPaused })),
 
   toggleActiveApplet: (applet) =>
@@ -62,9 +37,6 @@ const useGameStore = create<State>((set) => ({
       turn: 0,
       loading: true,
       isPaused: false,
-      isActionDrawerOpen: false,
-      isMapDrawerOpen: false,
-      selectedState: undefined,
       activeApplet: '',
     }),
 }));
