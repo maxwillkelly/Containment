6969290c262e3704321c8b9f93c48559f685b720
Max Kelly <maxwill.kelly@gmail.com>
2021-04-14 14:15:53 +0100

    Makes styling of PolicyDrawer match MapDrawer

--- src/components/game/PolicyDrawer.tsx
+++ src/components/game/PolicyDrawer.tsx
@@ -7,7 +7,7 @@ const PolicyDrawer = () => {
 
   return (
     <aside
-      className={`transform left-0 w-48 dark:bg-gray-700 dark:text-gray-200 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${asideClass}`}
+      className={`transform left-0 w-48 dark:bg-gray-700 dark:text-gray-200 border-r border-t border-gray-900 shadow fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${asideClass}`}
     >
       <div className="flex flex-col justify-center items-end">
         <h3 className="text-lg px-4 py-5">Policies</h3>
