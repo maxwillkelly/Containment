15d3ae03a60eda559b403ae9ef51168837896dbc
Max Kelly <maxwill.kelly@gmail.com>
2021-04-17 22:37:16 +0100

    Fixes bug in paused menu from last commit

--- src/components/game/PauseMenu.tsx
+++ src/components/game/PauseMenu.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import useGameStore from '../../stores/GameStore';
 import MenuItem from '../shared/MenuItem';
 
 const PauseMenu: React.FC = () => {
+  const history = useHistory();
   const togglePause = useGameStore((state) => state.togglePause);
 
   return (
@@ -11,7 +13,10 @@ const PauseMenu: React.FC = () => {
         <div className="px-5 py-14 w-9/12 rounded border dark:border-gray-300 dark:bg-gray-800 dark:text-gray-200">
           <h1 className="text-4xl font-bold text-center mb-5 mx-3">Paused</h1>
           <MenuItem name="Return to Game" handleClick={() => togglePause()} />
-          <MenuItem name="Exit to Main Menu" path="/" />
+          <MenuItem
+            name="Exit to Main Menu"
+            handleClick={() => history.push('/')}
+          />
         </div>
       </div>
     </div>
