98334e8f7ed778e873dea0e15b48995c578e6921
Max Kelly <maxwill.kelly@gmail.com>
2021-04-19 17:14:16 +0100

    Sets up political store to store popularity info

---
+++ src/stores/PoliticalStore.ts
@@ -0,0 +1,79 @@
+import lodash from 'lodash';
+import create from 'zustand';
+import { actions, Action } from '../data/actions';
+import { getHighestValueKey } from '../libs/object';
+import immer from './shared/immer';
+
+type State = {
+  base: number;
+  actionModifiers: Record<number, Record<string, number>>;
+
+  getPopularity: (turn: number) => number;
+
+  addActionModifier: (action: Action, turn: number) => void;
+  removeActionModifier: (action: Action, turn: number) => void;
+
+  advanceTurn: () => void;
+
+  reset: () => void;
+};
+
+const usePoliticalStore = create<State>(
+  immer((set, get) => ({
+    base: 55,
+    actionModifiers: {},
+
+    getPopularity: (turn) => {
+      const { base, actionModifiers } = get();
+      const turnActionModifiers = Object.values(actionModifiers[turn]);
+
+      const popularity = turnActionModifiers.reduce(
+        (curr, am) => curr + am,
+        base
+      );
+
+      return popularity;
+    },
+
+    addActionModifier: (action, turn) => {
+      const { id, graduationPercentage, impact } = action;
+
+      if (!graduationPercentage) return;
+
+      const modifier = impact.popularity(graduationPercentage);
+
+      set((state) => {
+        state.actionModifiers[turn + 1][id] = modifier;
+      });
+    },
+
+    removeActionModifier: (action, turn) =>
+      set((state) => delete state.actionModifiers[turn + 1][action.id]),
+
+    advanceTurn: () => {
+      const { actionModifiers } = get();
+      const turn = getHighestValueKey(actionModifiers);
+
+      set((state) => {
+        state.actionModifiers[turn + 1] = lodash.cloneDeep(
+          state.actionModifiers[turn]
+        );
+      });
+    },
+
+    reset: () => {
+      const enabledActions = actions.filter((a) => a.enabledByDefault);
+      const initialModifiers: Record<string, number> = {};
+
+      enabledActions.forEach((action) => {
+        initialModifiers[action.id] = action.impact.popularity(0.5);
+      });
+
+      set((state) => {
+        state.actionModifiers[0] = initialModifiers;
+      });
+    },
+  }))
+);
+
+export default usePoliticalStore;
