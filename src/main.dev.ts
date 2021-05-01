87869a106c684b4ce3649bc9b2a82080bb1cfcec
Max Kelly <maxwill.kelly@gmail.com>
2021-05-01 21:43:12 +0100

    Sound works in production + polish

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -69,6 +69,7 @@ const createWindow = async () => {
 
   mainWindow = new BrowserWindow({
     show: true,
+    fullscreen: true,
     width: 1024,
     height: 728,
     icon: getAssetPath('icon.png'),
@@ -77,6 +78,14 @@ const createWindow = async () => {
       contextIsolation: false,
     },
   });
+
+  mainWindow.setBackgroundColor('#27272a');
+
+  if (process.env.NODE_ENV === 'development') {
+    mainWindow.setFullScreen(false);
+  } else {
+    mainWindow.setMenuBarVisibility(false);
+  }
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
